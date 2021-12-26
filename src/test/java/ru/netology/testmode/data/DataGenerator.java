27befbcb5215c1aa9bf09f3b0b041c724e145674
Пастухова Ксения <colorleafs8@gmail.com>
2021-12-26 17:22:24 +0300

    Тест запуска тестов 5

--- src/test/java/ru/netology/testmode/data/DataGenerator.java
+++ src/test/java/ru/netology/testmode/data/DataGenerator.java
@@ -14,7 +14,7 @@ import static io.restassured.RestAssured.given;
 
 public class DataGenerator {
     private static final RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
+            .setBaseUri("http://127.0.0.1")
             .setPort(9999)
             .setAccept(ContentType.JSON)
             .setContentType(ContentType.JSON)
